a032e6e9473b755ecd538ab98a43a2d1c3fbdb8b
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-10-05 22:31:07 -0500

    serializerMethodfield

--- restframeworktut-project/api/serializers.py
+++ restframeworktut-project/api/serializers.py
@@ -25,7 +25,13 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
+    items = OrderItemSerializer(many=True, read_only=True)  ## Nested Serializer
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
     class Meta:
         model = Order
-        fields = ('order_id', 'created_at', 'user', 'status', 'items')
+        fields = ('order_id', 'created_at', 'user', 'status', 'items', 'total_price')
