22f22f7153f1a00fe35c1b2a7295ebba0e4c765d
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-10-05 21:55:10 -0500

    products list api

--- restframeworktut-project/api/views.py
+++ restframeworktut-project/api/views.py
@@ -1,13 +1,21 @@
 from django.http import JsonResponse
 from api.serializers import ProductSerializer
 from api.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 ## Function based Views
+# def product_list(request):
+#     products = Product.objects.all()
+#     serilizer = ProductSerializer(products, many=True)
+#     return JsonResponse({
+#         'data': serilizer.data
+#     })
 
+
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serilizer = ProductSerializer(products, many=True)
-    return JsonResponse({
-        'data': serilizer.data
-    })
+    return Response(serilizer.data)
